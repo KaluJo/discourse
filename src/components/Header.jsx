498a495f977001416e9689d742f230f6b1cce75f
Bhada Yun <bhadayun@gmail.com>
2024-03-02 15:27:01 -0800

    The Hack Begins

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            {/* Your component code here */}
+        </div>
+    );
+};
+
+export default Avatar;
---
+++ src/components/Textbox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Textbox = () => {
+    return (
+        <div>
+            {/* Your component code here */}
+        </div>
+    );
+};
+
+export default Avatar;
